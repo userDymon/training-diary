097d6d987634da397a72f54c8c6b804f111d95c2
Dmytro Hrabovskyi <dimagrabovskuy@gmail.com>
2023-12-09 13:33:17 +0200

    add login form

--- training-diary/main.cpp
+++ training-diary/main.cpp
@@ -1,4 +1,5 @@
 #include "mainwindow.h"
+#include "login.h"
 
 #include <QApplication>
 
@@ -6,6 +7,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
+    Login l;
+    l.show();
+    //w.show();
     return a.exec();
 }
